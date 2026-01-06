7d1e37ff8d1f0781699c75c9dd493a96a77d2f93
Matt Nightingale <matt.snail@gmail.com>
2026-01-06 19:48:20 +0000

    more css faff for mobile

--- src/Components/TechStack/TeckStack.jsx
+++ src/Components/TechStack/TeckStack.jsx
@@ -5,15 +5,13 @@ function TechStack() {
   return (
     <div className={styles.wrapper}>
       <h2 className={styles.title}>Tech Stack</h2>
-      <div className={styles.techContainer}>  
-        <div className={styles.techList}>
-          {skills.map((skill) => (
-            <div key={skill.name} className={styles.techItem}>
-              <p className={styles.techName}>{skill.name}</p>
-              <img src={skill.icon} alt={skill.name} className={styles.icon} />
-            </div>
-          ))}
-        </div>
+      <div className={styles.techList}>
+        {skills.map((skill) => (
+          <div key={skill.name} className={styles.techItem}>
+            <p className={styles.techName}>{skill.name}</p>
+            <img src={skill.icon} alt={skill.name} className={styles.icon} />
+          </div>
+        ))}
       </div>
     </div>
   );
