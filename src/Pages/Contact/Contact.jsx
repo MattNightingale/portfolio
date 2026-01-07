d1f657d71a9ff3ddabcbaef0b3dd18c6581c4793
Matt Nightingale <matt.snail@gmail.com>
2026-01-07 20:58:02 +0000

    started woking on conatct form

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,6 +1,5 @@
-import styles from './Contact.module.css';
-import { useNavigate } from 'react-router';
-
+import styles from "./Contact.module.css";
+import { useNavigate } from "react-router";
 
 function Contact() {
   const navigate = useNavigate();
@@ -12,10 +11,49 @@ function Contact() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.popup}>
-      <button className={styles.closeButton} onClick={handleClose}>&times;</button>
-      <h2 className={styles.title}>Contact Me</h2>
-      <p>You can reach me via email at example@example.com.</p>
-    </div>
+        <button className={styles.closeButton} onClick={handleClose}>
+          &times;
+        </button>
+        <h2 className={styles.title}>Contact Me</h2>
+        <p>I'd love to hear from you! Please fill out the form below or connect with me on LinkedIn or Github:</p>
+        <div className={styles.form}>
+          <form>
+            <label className={styles.label}>
+              Name:
+              <input type="text" name="name" className={styles.input} />
+            </label>
+            <label className={styles.label}>
+              Email:
+              <input type="email" name="email" className={styles.input} />
+            </label>
+            <label className={styles.label}>
+              Message:
+              <textarea name="message" className={styles.textarea}></textarea>
+            </label>
+            <button type="submit" className={styles.submitButton}>Send</button>
+          </form>
+          </div>
+        <div className={styles.links}>
+          <a
+            href="https://www.linkedin.com/in/matt-nightingale-925639129/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="assets/linkedin.png"
+              alt="LinkedIn"
+              className={styles.linkedinIcon}
+            />
+          </a>
+          <a
+            href="https://github.com/MattNightingale"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="assets/github.png" alt="GitHub" className={styles.githubIcon} />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
