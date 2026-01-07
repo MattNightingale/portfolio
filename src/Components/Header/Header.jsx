78376a44782a251293224418fba8d9a9c204c80f
Matt Nightingale <matt.snail@gmail.com>
2026-01-07 16:19:56 +0000

    adding burgermenu for mobile

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,21 +1,25 @@
 import { SiteNav } from "../SiteNav/SiteNav";
 import styles from "./Header.module.css";
+import { slide as Menu } from "react-burger-menu";
 
 export function Header() {
   return (
     <header className={styles.wrapper}>
       <div className={styles.headerContent}>
-      <div className={styles.headerLeft}>
-        <img
-          className={styles.logo}
-          src="/assets/mnlogovector.png"
-          alt="Matt Nightingale Logo"
-        />
-        <h1>Matt Nightingale</h1>
-      </div>
-      <div className={styles.headerRight}>
-        <SiteNav />
-      </div>
+        <div className={styles.headerLeft}>
+          <img
+            className={styles.logo}
+            src="/assets/mnlogovector.png"
+            alt="Matt Nightingale Logo"
+          />
+          <h1 className={styles.title}>Matt Nightingale</h1>
+        </div>
+        <div className={styles.headerRight}>
+          <SiteNav />
+        </div>
+        <Menu right className={styles.burgerMenu}>
+          <SiteNav />
+        </Menu>
       </div>
     </header>
   );
