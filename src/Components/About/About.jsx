1215488ba4596a296cc94d05f01a0c6271b04c08
Matt Nightingale <matt.snail@gmail.com>
2026-01-10 18:09:53 +0000

    added vercel path fix and more about info

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,13 +1,16 @@
 import TechStack from "../../Components/TechStack/TeckStack";
 import styles from "./About.module.css";
-import { SlideInFromLeft, SlideInFromRight, SlideInFromTop } from "../Animations/Animations";
-
+import {
+  SlideInFromLeft,
+  SlideInFromRight,
+  SlideInFromTop,
+} from "../Animations/Animations";
 
 function About() {
   return (
     <div className={styles.wrapper}>
-      <SlideInFromLeft fullWidth >
-      <h2 className={styles.title}>About</h2>
+      <SlideInFromLeft fullWidth>
+        <h2 className={styles.title}>About</h2>
       </SlideInFromLeft>
       <SlideInFromTop delay={0.2}>
         <img
@@ -16,7 +19,7 @@ function About() {
           className={styles.portrait}
         />
       </SlideInFromTop>
-      <SlideInFromRight delay={0.4}>
+      <SlideInFromRight delay={0.4} viewAmount={0.05}>
         <div className={styles.content}>
           <div className={styles.text}>
             <p>
@@ -29,9 +32,17 @@ function About() {
             <p>
               I have a naturally analytical mindset, a passion for learning, and
               thrive in roles that involve building, improving, and
-              collaborating. I’m particularly motivated by opportunities to grow
-              as a developer within a modern, exciting and agile engineering
-              team.
+              collaborating. As an experienced, proven team player and a
+              dedicated developer, I bring a strong work ethic and collaborative
+              spirit to every project. I’m particularly motivated by
+              opportunities to grow as a developer within a modern, exciting and
+              agile engineering team.
+            </p>
+            <p>
+              When I’m not coding, you’ll find me in the mountains, cycling,
+              skiing or generally adventuring, or in the kitchen working on new
+              recipies. I’m an avid reader and lifelong learner, always
+              keen to explore new technologies and engineering concepts.
             </p>
           </div>
         </div>
