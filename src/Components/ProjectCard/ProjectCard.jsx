6a9ec97a5883332e9e1dc5671c0f8ca0eb0d8e3a
Matt Nightingale <matt.snail@gmail.com>
2026-01-11 12:24:06 +0000

    reverted back to original card layout

--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -1,28 +1,9 @@
 import styles from "./ProjectCard.module.css";
-import { useEffect, useState } from "react";
 
 export function ProjectCard({ title, description, link, image, tech, github }) {
-  const [isWideScreen, setIsWideScreen] = useState(
-    () => window.innerWidth >= 1024
-  );
-
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(min-width: 1024px)");
-    const handleMediaChange = (e) => setIsWideScreen(e.matches);
-    setIsWideScreen(mediaQuery.matches);
-    mediaQuery.addEventListener("change", handleMediaChange);
-    return () => mediaQuery.removeEventListener("change", handleMediaChange);
-  }, []);
-
-  const imageSrc = isWideScreen
-    ? `/assets/${title.toLowerCase().replace(/[\s']/g, "")}wide.png`
-    : `/assets/${title.toLowerCase().replace(/[\s']/g, "")}mob.png`;
-
   return (
-    <div
-      className={styles.wrapper}
-      style={{ backgroundImage: `url(${imageSrc})` }}
-    >
+    <div className={styles.wrapper}>
+      <img src={image} alt={title} className={styles.image} />
       <div className={styles.content}>
         <h3 className={styles.title}>{title}</h3>
         <p className={styles.description}>{description}</p>
